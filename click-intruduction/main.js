50eb352f2fc932b6c5b4b70e249408b86a578c70
clouddawn <1993456181@qq.com>
2021-04-25 10:55:48 +0800

    生活万岁

---
+++ click-intruduction/main.js
@@ -0,0 +1,5 @@
+let life = document.querySelector('#life');
+const life_introduction = '这是一部2018年上映的真实题材电影，讲述了十五名普通中国人在2017年真实的生活状态。并无交集的十四段故事谱写着同一个世界角落的生活，它或许艰辛，或许无奈，或许也带着些许苦涩，但每个人都在默默坚持着、爱着这样的生活。'
+life.addEventListener('click',function(){
+    console.log(life_introduction);
+});
